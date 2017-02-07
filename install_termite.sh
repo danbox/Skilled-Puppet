45ff899f566b06698e70603a4efc735e6ca390c5
Dan Boxler <danbox19@msn.com>
2017-02-06 21:33:25 -0700

    adding a new dependency

--- install_termite.sh
+++ install_termite.sh
@@ -5,6 +5,7 @@ git clone https://github.com/thestinger/vte-ng.git
 sudo apt-get install g++ libgtk-3-dev gtk-doc-tools gnutls-bin valac
 sudo apt-get install libglib3.0-cil-dev libgnutls28-dev libgirepository1.0-dev
 sudo apt-get install libxml2-utils gperf
+sudo apt-get install libpcre2-dev
 cd vte-ng && ./autogen.sh && make && sudo make install
 cd ../termite && make && sudo make install
 sudo desktop-file-install termite.desktop
