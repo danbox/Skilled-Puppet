ccda75085d2b11b803e0964bdc2b22e741f746c8
Dan Boxler <danbox19@msn.com>
2017-08-03 18:29:33 -0600

    .zshrc and .gitignore added

---
+++ install_zsh.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# must be run as root
+if [[ $EUID -ne 0  ]]; then
+    echo "This script must be run as root" 
+    exit 1
+fi
+
+# install zsh and oh.my.zsh
+sudo apt-get -f install zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# font config
+wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+sudo mv PowerlineSymbols.otf /usr/share/fonts/
+sudo fc-cache -vf
+sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
