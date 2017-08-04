46de0f357d1784d1f5575ddd1631ecbb3ad066a2
Dan Boxler <danbox19@msn.com>
2017-08-03 18:49:56 -0600

    Adding new script to install spf13 and adding force flags to termite script

---
+++ install_spf13.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Install spf13
+curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
--- install_termite.sh
+++ install_termite.sh
@@ -2,10 +2,10 @@
 
 git clone --recursive https://github.com/thestinger/termite.git
 git clone https://github.com/thestinger/vte-ng.git
-sudo apt-get install g++ libgtk-3-dev gtk-doc-tools gnutls-bin valac
-sudo apt-get install libglib3.0-cil-dev libgnutls28-dev libgirepository1.0-dev
-sudo apt-get install libxml2-utils gperf
-sudo apt-get install libpcre2-dev
+sudo apt-get install -f g++ libgtk-3-dev gtk-doc-tools gnutls-bin valac
+sudo apt-get install -f libglib3.0-cil-dev libgnutls28-dev libgirepository1.0-dev
+sudo apt-get install -f libxml2-utils gperf
+sudo apt-get install -f libpcre2-dev
 cd vte-ng && ./autogen.sh && make && sudo make install
 cd ../termite && make && sudo make install
 sudo desktop-file-install termite.desktop
